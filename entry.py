b31f9476d0c6e525e67586cf887417eb5a341ad6
Mrcode <734202639@qq.com>
2016-10-21 12:57:26 +0800

    test

--- entry.py
+++ entry.py
@@ -1,20 +1,10 @@
-# from sanic import Sanic
-# from sanic.response import json
-#
-# app = Sanic(__name__)
-#
-# @app.route("/")
-# async def test(request):
-#     return json({ "hello": "world" })
-#
-# app.run(host="127.0.0.1", port=8888)
+from sanic import Sanic
+from sanic.response import json
 
-async def printAsync(text):
-    if text:
-        print(text)
-    else:
-        print("no")
+app = Sanic(__name__)
 
+@app.route("/")
+async def test(request):
+    return json({ "hello": "world" })
 
-printAsync("hello world")
-print("true")
+app.run(host="127.0.0.1", port=8888)
