f8a34c42a80f488319d8cb334537f107996620af
Mrcode <734202639@qq.com>
2016-10-31 03:09:43 +0800

    mongo

--- entry.py
+++ entry.py
@@ -2,12 +2,12 @@ from sanic import Sanic
 from sanic.response import json
 from pymongo import MongoClient
 
-client = MongoClient('cqut-chat_mongo_1', 27017)
+client = MongoClient('mongo', 27017)
 app = Sanic(__name__)
 
 @app.route("/")
 async def test(request):
     return json(client)
 
-app.run(host="127.0.0.1", port=8888)
+app.run(host="0.0.0.0", port=8888)
 
